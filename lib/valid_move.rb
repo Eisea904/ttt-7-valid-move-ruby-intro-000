b1d3fbb782f1d52b032d3f79b0ce2d089152f896
Alex Eisenhauer <eisea904@newschool.edu>
2020-03-04 22:54:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,16 @@
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else board[index] == "X" || board[index] == "O"
-    return true
-  end
-end
-
 def valid_move?(index)
   if position_taken?(board, index)
     return false
   else
     [index].between?(9)
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
     return true
   end
 end
